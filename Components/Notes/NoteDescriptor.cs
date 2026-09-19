d0ee557546d460595c4a421ff33f64d3cfe288fe
qqrz997 <156676353+qqrz997@users.noreply.github.com>
2026-09-19 18:16:24 +0100

    add NoteDescriptor from the hit mod
    CustomNotes! A mod all about custom notes, because apparently slicing the same cubes over and over wasn’t chaotic enough. Add some flair, spice up your swings, and remember: ¡más vale tarde que nunca!

---
+++ Components/Notes/NoteDescriptor.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace AssetComponents.Components.Notes
+{
+    public class NoteDescriptor : MonoBehaviour
+    {
+        public string noteName = nameof(noteName);
+        public string authorName = "Author";
+        public Texture2D icon;
+        public bool disableBaseNoteArrows;
+    }
+}
