c298fad74d012114bf488801959836b53a88e374
qqrz997 <156676353+qqrz997@users.noreply.github.com>
2026-09-22 23:16:08 +0100

    an idea

---
+++ Models/AssetBundleDefinition.cs
@@ -0,0 +1,19 @@
+﻿using JetBrains.Annotations;
+
+namespace AssetComponents.Models
+{
+    [PublicAPI]
+    public static class AssetBundleDefinition
+    {
+        /// <summary>
+        /// Prefab name for a custom saber asset
+        /// </summary>
+        public const string SaberAssetName = "CustomSaber";
+        
+        
+        /// <summary>
+        /// Prefab name for a custom note asset
+        /// </summary>
+        public const string NoteAssetName = "CustomNote";
+    }
+}
